5aa579e707392c62368120faf00321e12901fa27
kabeerSohail7 <kabeersohel@gmail.com>
2021-04-17 08:21:02 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import React,{useEffect} from 'react';
+import Pusher from 'pusher-js';
 
 
 function App() {
+
+  useEffect(()=>{
+    const pusher = new Pusher('2a302de178aaf5fc1a6b', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data) => {
+      alert(JSON.stringify(data));
+    });
+  },[])
+
   return (
     <div className="app">
       <div className="app__body" >
